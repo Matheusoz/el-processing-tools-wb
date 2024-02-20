71ec711831f3b9cf475fb0cccac3667e052a2c9b
Matheusoz <140904413+Matheusoz@users.noreply.github.com>
2024-02-20 19:25:52 +0000

    Update script.user.js

--- src/wb_cleaning/extraction/jdc_tags_extractor.py
+++ src/wb_cleaning/extraction/jdc_tags_extractor.py
@@ -7,4 +7,19 @@ from wb_cleaning.dir_manager import get_data_dir
 from wb_cleaning.translate import translation
 
 ACCENTED_CHARS = set(
- 
+    "ÂÃÄÀÁÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûüýþÿ")
+
+jdc_tags_processor = KeywordProcessor()
+jdc_tags_processor.set_non_word_boundaries(
+    jdc_tags_processor.non_word_boundaries | ACCENTED_CHARS)
+
+inflect_engine = inflect.engine()
+
+# input schema
+# -> tag_value
+# -> tag_prototypes
+
+# Definition of data input:
+# The input to the tag extractor is an excel or csv file.
+# The first column of the data must be the intended tag keyword.
+# To remove ambiguity, a header with name "tag_keyword" must be present.
