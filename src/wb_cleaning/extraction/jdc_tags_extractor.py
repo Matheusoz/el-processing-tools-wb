02f9898a3b007c23c17a0a0a14909ded6b982ae1
Matheusoz <140904413+Matheusoz@users.noreply.github.com>
2024-02-21 06:23:56 +0000

    readme adjusted

--- src/wb_cleaning/extraction/jdc_tags_extractor.py
+++ src/wb_cleaning/extraction/jdc_tags_extractor.py
@@ -23,3 +23,14 @@ inflect_engine = inflect.engine()
 # The input to the tag extractor is an excel or csv file.
 # The first column of the data must be the intended tag keyword.
 # To remove ambiguity, a header with name "tag_keyword" must be present.
+# Additionally, all non-empty values in the columns to the right of the tag keyword are considered as prototypes.
+# Occurences of these prototypes will be mapped to the tag keyword.
+
+
+def get_keywords_mapping(tags_sheet, src="en", translate_to=None):
+    # translate_to = ["fr", "es"]
+    if translate_to is None:
+        translate_to = []
+
+    tags_mapping = tags_sheet.set_index("tag_keyword").T.apply(
+        # If prototypes have "underscores" create a copy with the underscore replaced with 
