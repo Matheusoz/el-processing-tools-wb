dd4425ad14845d5de27d903a77ec9bc64238b049
Matheusoz <140904413+Matheusoz@users.noreply.github.com>
2024-03-10 11:22:12 +0000

    add travis build file

--- src/wb_cleaning/extraction/whitelist/mappings.py
+++ src/wb_cleaning/extraction/whitelist/mappings.py
@@ -3,4 +3,20 @@ from wb_cleaning.extraction import whitelist
 
 
 def get_countries_mapping():
-    country_df = pd.read_csv(whitelist.get_country_csv(), index_col=0, he
+    country_df = pd.read_csv(whitelist.get_country_csv(), index_col=0, header=None)
+
+    country_mapping = {}
+    for country_code, names in country_df.iterrows():
+        names = names.dropna().values
+        for name in names:
+            country_mapping[name] = {'code': country_code, 'normalized': names[0]}
+
+    return country_mapping
+
+
+def get_wb_presidents_mapping():
+    wb_presidents_df = pd.read_csv(whitelist.get_wb_presidents_csv(), index_col=None, header=None)
+
+    wb_presidents_mapping = {}
+    for _, names in wb_presidents_df.iterrows():
+        names = names.drop
