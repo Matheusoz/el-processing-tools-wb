2684c64f5b69b2289f31a4570df83beac3bd4490
Matheusoz <140904413+Matheusoz@users.noreply.github.com>
2024-04-21 12:50:37 +0000

    updating the docs

--- tests/extraction/test_country_extractor.py
+++ tests/extraction/test_country_extractor.py
@@ -9,4 +9,21 @@ class TestCountryExtractor:
             code,
             "asean+countries_data",
             "ASEAN COUNTRIES_DATA",
-            "ASEAN+COUN
+            "ASEAN+COUNTRIES DATA",
+        ]
+        returns = ce.get_normalized_country_group_name(code)
+
+        assert expected == returns
+
+    def test_get_country_name_from_code(self):
+        code = "PHL"
+        expected = "Philippines"
+        returns = ce.get_country_name_from_code(code)
+
+        assert expected == returns
+
+    def test_load_iso3166_3_country_info(self):
+        assert isinstance(ce.load_iso3166_3_country_info(), dict)
+
+    def test_load_country_groups_map(self):
+        expected = ['BRN',
